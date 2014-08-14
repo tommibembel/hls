0ceb983d2046f91572ae0211f26a9b0501633c41
Tommi Bembel <tommi.bembel@gmail.com>
2014-08-14 18:44:23 +0200

    ADDED GPIO support

--- install/apt.sh
+++ install/apt.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get -y upgrade
-apt-get -y install mysql-server python-mysqldb python-mysqldb-dbg
-
-
+apt-get -y install mysql-server python-mysqldb python-mysqldb-dbg python-rpi.gpio python-dev
+# No NEED of following files
 apt-get -y install vim
 update-alternatives --set editor /usr/bin/vim.tiny
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 
 import os
@@ -10,12 +10,12 @@ import MySQLdb
 import daemon
 import locale
 import ConfigParser
+#import RPi.GPIO
 
 scriptname = os.path.basename(__file__)
 
 bitlvl = float(15)
 factor = 100 / bitlvl
-print factor
 
 confparser = ConfigParser.SafeConfigParser()
 if os.path.isfile("/etc/hls.conf"):
@@ -23,9 +23,14 @@ if os.path.isfile("/etc/hls.conf"):
 elif os.path.isfile(os.path.join(os.path.dirname(os.path.realpath(__file__)),"hls.conf")):
         confparser.read(os.path.join(os.path.dirname(os.path.realpath(__file__)),"hls.conf"))
 logfile = confparser.get("main", "logfile")
-dbname = confparser.get("main", "dbname")
 defprof = confparser.get("main", "default_profile")
-
+sleeptime = int(confparser.get("main", "sleeptime"))
+dbhost = confparser.get("database", "dbhost")
+dbname = confparser.get("database", "dbname")
+dbuser = confparser.get("database", "dbuser")
+dbpasswd = confparser.get("database", "dbpasswd")
+pins = confparser._sections["pins"]
+pins.pop("__name__", None)
 
 argparser = argparse.ArgumentParser(description="Haus-Lüfter-Steuerung" , epilog="Programm für die Steuerung eines Lüfters über Zeit- und Leistungsvorgaben.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 argparser.add_argument("-i", "--initdb", help="Erstellt Datenbank und Tabellen-Schema.", action="store_true")
@@ -50,24 +55,21 @@ elif args.verbose == 1:
 #elif args.verbose >= 3:
   logger.setLevel(logging.DEBUG)
 
-def initdb(dbname=dbname):
-  conn = MySQLdb.connect(dbname)
+def initdb():
+  conn = MySQLdb.connect(host=dbhost, user=dbuser, passwd=dbpasswd, db=dbname)
   c = conn.cursor()
-  c.execute("SELECT name FROM sqlite_master WHERE type='table';")
-  t =  c.fetchall()
-  for i in t:
-    c.execute("DROP TABLE %s;"%i[0])
+  c.execute("DROP TABLE IF EXISTS schedules;")
   c.execute("CREATE TABLE schedules (stime time, etime time, power int, dayofweek int, profile int);")
+  c.execute("DROP TABLE IF EXISTS profiles;")
   c.execute("CREATE TABLE profiles (id int, name text, active int);")
-  c.execute("CREATE TABLE days (id int, name text);")
   c.execute("INSERT INTO profiles VALUES (1, 'manual', 0);")
-  c.execute("INSERT INTO profiles VALUES (2, ?, 1);",(defprof,))
+  c.execute("INSERT INTO profiles VALUES (2, '%s', 1);"%defprof)
   conn.commit()
   conn.close()
   print "Datenbank initialisiert und Tabellen angelegt."
 
 def gettime():
-  locale.setlocale(locale.LC_TIME, "de_DE")
+  #locale.setlocale(locale.LC_TIME, "de_DE")
   d = datetime.date.today().weekday()
   t = datetime.datetime.now().strftime("%H:%M")
   return (d, t)
@@ -75,28 +77,61 @@ def gettime():
 def getpower(date, profile):
   d = str(date[0])
   t = str(date[1])
-  conn = MySQLdb.connect(dbname)
+  conn = MySQLdb.connect(host=dbhost, user=dbuser, passwd=dbpasswd, db=dbname)
   c = conn.cursor()
-  c.execute("SELECT power FROM schedules WHERE dayofweek=? AND stime<=? AND etime>=? AND profile=?;",(d,t,t,profile))
+  print "SELECT power FROM schedules WHERE dayofweek=%s AND stime<='%s' AND etime>='%s' AND profile=%s;"%(d,t,t,profile)
+  c.execute("SELECT power FROM schedules WHERE dayofweek=%s AND stime<='%s' AND etime>='%s' AND profile=%s;"%(d,t,t,profile))
   res = c.fetchall()
   conn.close()
   return int(res[0][0])
 
 def getactiveprofile():
-  conn = MySQLdb.connect(dbname)
+  conn = MySQLdb.connect(host=dbhost, user=dbuser, passwd=dbpasswd, db=dbname)
   c = conn.cursor()
   c.execute("SELECT id FROM profiles WHERE active=1;")
   res = c.fetchall()
   conn.close()
   return int(res[0][0])
 
-def binarize(val):
-  print val
-  return '{0:04b}'.format(val)
-  
 def factorize(val):
   res = val / factor
   return int(res)
+
+def gpio_setup():
+  RPi.GPIO.setmode(RPi.GPIO.BOARD)
+  for pin in pins.keys():
+    RPi.GPIO.setup(pins[pin], RPi.GPIO.OUT)
+
+def gpio_set(val):
+  if (val - 8) >= 0:
+    RPi.GPIO.output(pins["8"], RPi.GPIO.HIGH)
+    logger.debug("Set power to %s."%pins["8"])
+    val = val - 8
+  else:
+    RPi.GPIO.output(pins["8"], RPi.GPIO.LOW)
+    logger.debug("Unset power to %s."%pins["8"])
+  if (val - 4) >= 0:
+    RPi.GPIO.output(pins["8"], RPi.GPIO.HIGH)
+    logger.debug("Set power to %s."%pins["4"])
+    val = val - 4
+  else:
+    RPi.GPIO.output(pins["8"], RPi.GPIO.LOW)
+    logger.debug("Unset power to %s."%pins["4"])
+  if (val - 2) >= 0:
+    RPi.GPIO.output(pins["8"], RPi.GPIO.HIGH)
+    logger.debug("Set power to %s."%pins["2"])
+    val = val - 2
+  else:
+    RPi.GPIO.output(pins["8"], RPi.GPIO.LOW)
+    logger.debug("Unset power to %s."%pins["2"])
+  if (val - 1) >= 0:
+    RPi.GPIO.output(pins["8"], RPi.GPIO.HIGH)
+    logger.debug("Set power to %s."%pins["1"])
+    val = val - 1
+  else:
+    RPi.GPIO.output(pins["8"], RPi.GPIO.LOW)
+    logger.debug("Unset power to %s."%pins["1"])
+  
 
 class daemonize(daemon.Daemon):
   def run(self):
@@ -104,14 +139,14 @@ class daemonize(daemon.Daemon):
       zeit = gettime()
       res = getpower(zeit,getactiveprofile())
       res = factorize(res)
-      res = binarize(res)
       print res
       logger.debug("Get powerlevel of %s"%res)
-      time.sleep(60)
+      gpio_set(res)
+      time.sleep(sleeptime)
       if args.test:
         break
 
-d = daemonize("/var/run/hls.pid")
+d = daemonize("hls.pid")
 if args.initdb:
   initdb()
 if args.daemonize:
